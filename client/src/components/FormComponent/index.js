fe9c09266a055e54af1add2317477ccc8ca08e36
Jose Mejia <aalejandro.mreyes@icloud.com>
2019-06-25 17:32:41 -0400

    Initial Commint

--- client/src/components/FormComponent/index.js
+++ client/src/components/FormComponent/index.js
@@ -1,20 +1,28 @@
 import React from 'react';
-import { Button, Checkbox, Form } from 'semantic-ui-react'
+import { Button, Input, Label, Form } from 'semantic-ui-react'
 
 export default class EventsForm extends React.Component {
   render() {
     return (
       <Form>
         <Form.Field>
-          <label>First Name</label>
-          <input placeholder='First Name' />
+          <label>Event Name</label>
+          <input placeholder='Name' name="name" />
         </Form.Field>
         <Form.Field>
-          <label>Last Name</label>
-          <input placeholder='Last Name' />
+          <label>Description</label>
+          <input placeholder='Description' name="description" />
         </Form.Field>
         <Form.Field>
-          <Checkbox label='I agree to the Terms and Conditions' />
+          <label>Budget</label>
+          <input placeholder='Budget' name="Budget" />
+        </Form.Field>
+        <Form.Field>
+          <Input labelPosition='right' type='text' placeholder='Amount'>
+            <Label basic>$</Label>
+            <input />
+            <Label>.00</Label>
+          </Input>
         </Form.Field>
         <Button type='submit'>Submit</Button>
       </Form>
