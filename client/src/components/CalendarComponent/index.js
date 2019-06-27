87aa2de585b83b2c7ce70d5fce9f570a4abc12aa
Jose Mejia <aalejandro.mreyes@icloud.com>
2019-06-27 12:13:09 -0400

    Adds event to calendar when created

--- client/src/components/CalendarComponent/index.js
+++ client/src/components/CalendarComponent/index.js
@@ -5,7 +5,16 @@ import "react-big-calendar/lib/sass/styles.scss";
 import ModalModalExample from "./Modal";
 import { connect } from "react-redux";
 import { Notify } from "react-redux-notify";
+import { getEvents } from "../../actions/EventsAction";
+import { Card, Button } from "semantic-ui-react";
+
 const localizer = momentLocalizer(moment);
+
+let components = {
+  // eventWrapper: Card // used by each view (Month, Day, Week),
+  // dateCellWrapper: Card
+  // toolbar: Button
+};
 
 class CalendarEvents extends React.Component {
   state = {
@@ -13,19 +22,22 @@ class CalendarEvents extends React.Component {
     event: {
       eventStarts: "",
       eventEnds: "",
-      title: "",
+      title: ""
     }
   };
 
+  componentDidMount() {
+    this.props.getEvents();
+  }
 
   closeModal = () => {
     return new Promise((resolve, reject) => {
       this.setState({
         modal: false
-      })
+      });
       resolve();
-    })
-  }
+    });
+  };
 
   render() {
     return (
@@ -33,7 +45,13 @@ class CalendarEvents extends React.Component {
         <Notify />
         <Calendar
           localizer={localizer}
-          events={this.props.events}
+          events={this.props.events.map((event, index) => ({
+            id: index,
+            title: event.name,
+            allDay: false,
+            start: new Date(event.date.split("|")[0]),
+            end: new Date(event.date.split("|")[1])
+          }))}
           startAccessor="start"
           endAccessor="end"
           onSelectSlot={e => {
@@ -46,14 +64,14 @@ class CalendarEvents extends React.Component {
             });
           }}
           selectable
-          defaultView='week'
+          defaultView="week"
+          onDoubleClickEvent={(event, e) => console.log(event, e)}
+          components={components}
         />
         <ModalModalExample
           modal={this.state.modal}
           onClose={this.closeModal}
-          eventStarts={`${
-            this.state.event.eventStarts.toLocaleString()
-            }`}
+          eventStarts={`${this.state.event.eventStarts.toLocaleString()}`}
           eventEnds={`${this.state.event.eventEnds.toLocaleString()}`}
         />
       </div>
@@ -67,5 +85,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  {}
+  { getEvents }
 )(CalendarEvents);
